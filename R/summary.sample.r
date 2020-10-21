846f375212e051b8484dbbfc9bc758e541f1c9f4
david-borchers <dlb@st-andrews.ac.uk>
2020-10-21 12:30:09 +0100

    changed summary.sample() to print ony first nmax samples

--- R/summary.sample.r
+++ R/summary.sample.r
@@ -1,15 +1,21 @@
 #' @export
-summary.sample<-function(dat)
+summary.sample<-function(dat,nmax=30)
 {
   if(!is.sample(dat)) stop("Argument 'dat' must be of class 'sample'")
   if(dat$M>dat$m) {
+    sumunits = dat$units
+    nunits = length(sumunits)
+    if(nunits>nmax) {
+      sumunits = sumunits[1:nmax]
+      warning(paste("Only first ",nmax," units shown. Increase nmax to show more.", sep=""))
+    }
     cat("\n         Cluster Sample Summary")
     cat("\n=============================================")
     cat("\n                           number of clusters: ",dat$M)
     cat("\n                   number of clusters sampled: ",dat$m)
     #    cat("\n                            cluster sizes (N): ",dat$N)
     #    cat("\n                   mean N across all clusters: ",dat$mu.x)
-    cat("\n                             clusters sampled: ",dat$units)
+    cat("\n                             clusters sampled: ",sumunits)
     cat("\n response variable totals in sampled clusters: ",format(dat$y.value))
     cat("\n                   mean N in sampled clusters: ",mean(dat$x.value))
     cat("\n=============================================\n")
@@ -17,11 +23,17 @@ summary.sample<-function(dat)
     cat("\n      Stratified random Sample Summary")
     cat("\n=============================================")
     for(i in 1:dat$M) {
+      sumunits = dat[[i]]$units
+      nunits = length(sumunits)
+      if(nunits>nmax) {
+        sumunits = sumunits[1:nmax]
+        warning(paste("Stratum",i,". Only first ",nmax," units shown. Increase nmax to show more.", sep=""))
+      }
       cat("\n Stratum ",i)
       cat("\n---------------")
       cat("\n                          population size (N): ",dat[[i]]$N)
       cat("\n                              sample size (n): ",dat[[i]]$n)
-      cat("\n                   population units in sample: ",dat[[i]]$units)
+      cat("\n                   population units in sample: ",sumunits)
       cat("\n             sample mean of response variable: ",format(mean(dat[[i]]$y.value)))
       if(is.null(dat[[i]]$mu.x)) {
         cat("\n (no auxiliary variable)")
@@ -33,11 +45,17 @@ summary.sample<-function(dat)
     }
     cat("\n=============================================\n")
   } else {
+    sumunits = dat[[1]]$units
+    nunits = length(sumunits)
+    if(nunits>nmax) {
+      sumunits = sumunits[1:nmax]
+      warning(paste("Only first ",nmax," units shown. Increase nmax to show more.", sep=""))
+    }
     cat("\n      Simple random Sample Summary")
     cat("\n=============================================")
     cat("\n                          population size (N): ",dat$N)
     cat("\n                              sample size (n): ",dat$n)
-    cat("\n                   population units in sample: ",dat[[1]]$units)
+    cat("\n                   population units in sample: ",sumunits)
     cat("\n             sample mean of response variable: ",format(mean(dat[[1]]$y.value)))
     if(is.null(dat[[1]]$mu.x)) {
       cat("\n (no auxiliary variable)")
