9a4c8022e8621b8c8cf73f445e7b55cf0731a123
david-borchers <dlb@st-andrews.ac.uk>
2022-09-07 09:53:12 +0100

    updated installscript

--- inst/installscript.R
+++ inst/installscript.R
@@ -1,2 +1,2 @@
-install.packages("devtools")
+if (!require("devtools", quietly = TRUE, character.only = TRUE)) install.packages("devtools")
 devtools::install_github("david-borchers/sampling",build = TRUE)
